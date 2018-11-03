a893bd5b61e305efd131e1e28a9bf705375cc8c5
gjh <garyjh99@gmail.com>
2018-11-03 12:51:04 -0400

    added tab bar vc

---
+++ Controllers/HomeScreenViewController.swift
@@ -0,0 +1,32 @@
+//
+//  HomeScreenViewController.swift
+//  DEXServiceTool
+//
+//  Created by Gary Harris on 11/3/18.
+//  Copyright © 2018 Gary Harris. All rights reserved.
+//
+
+import UIKit
+
+class HomeScreenViewController: UIViewController {
+    @IBOutlet weak var backView: UIView!
+    @IBOutlet weak var controlView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Controllers/LoginViewController.swift
@@ -0,0 +1,44 @@
+//
+//  LoginViewController.swift
+//  DEXServiceTool
+//
+//  Created by Gary Harris on 11/3/18.
+//  Copyright © 2018 Gary Harris. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var userNameTxt: UITextField!
+    @IBOutlet weak var pswdTxt: UITextField!
+    @IBOutlet weak var loginBtn: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    
+    @IBAction func didTapLoginButton(_ sender: UIButton) {
+        
+        let sb = UIStoryboard(name: "Main", bundle: nil)
+        let tabBarVC: MainTabBarViewController = sb.instantiateViewController(withIdentifier: "IDMainTabBarViewController") as! MainTabBarViewController
+        
+        self.navigationController?.pushViewController(tabBarVC, animated: true)
+    }
+    
+}
